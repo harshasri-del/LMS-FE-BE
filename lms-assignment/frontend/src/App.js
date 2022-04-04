0bb0ece2a5d3ae0d3ef5791486d2a7415e950c7b
harshasri-del <57004663+harshasri-del@users.noreply.github.com>
2022-04-04 21:03:06 +0530

    uploaded fe and be

---
+++ lms-assignment/frontend/src/App.js
@@ -0,0 +1,27 @@
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import "./App.css";
+import Login from "./pages/Login/Login";
+import "primereact/resources/themes/lara-light-indigo/theme.css";
+import "primereact/resources/primereact.min.css";
+import "primeicons/primeicons.css";
+import AdminHome from "./pages/AdminHome/AdminHome";
+import StudentHome from "./pages/StudentHome/StudentHome";
+import Header from "./components/Header/Header";
+
+function App() {
+	return (
+		<div className="app">
+			<Router>
+			<Header />
+				<Routes>
+					<Route path="login" element={<Login />} />
+					<Route path="admin/*" element={<AdminHome />} />
+					<Route path="student/*" element={<StudentHome/>}/> 
+					
+				</Routes>
+			</Router>
+		</div>
+	);
+}
+
+export default App;
